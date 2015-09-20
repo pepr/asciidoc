5d8a129c6de612763ccdf00c090e15d46d80572c
Petr Prikryl <prikryl@atlas.cz>
2015-09-20 12:27:04 +0200

    AsciiDoc_big_picture Mind Map: encoding, themes...
    - enforcing encoding from command line works
      `--attribute encoding=windows-1250`
    - the :encoding: in the source file does not work

--- AsciiDoc_big_picture.mm
+++ AsciiDoc_big_picture.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.3.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="AsciiDoc -- big picture" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1442487901439" MIN_WIDTH="2"><hook NAME="MapStyle">
+<node TEXT="AsciiDoc -- big picture" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1442736634497" MIN_WIDTH="2"><hook NAME="MapStyle">
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node">
@@ -49,8 +49,8 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="1"/>
-<richcontent TYPE="DETAILS">
+<hook NAME="AutomaticEdgeColor" COUNTER="4"/>
+<richcontent TYPE="DETAILS" HIDDEN="true">
 
 <html>
   <head>
@@ -63,6 +63,21 @@
   </body>
 </html>
 </richcontent>
+<node TEXT="notes based on User Guide" POSITION="right" ID="ID_1370446767" CREATED="1442738088823" MODIFIED="1442738216771">
+<edge COLOR="#660000" WIDTH="2"/>
+<node TEXT="backends" ID="ID_1698496585" CREATED="1442738142365" MODIFIED="1442738174411">
+<node TEXT="backend plugins" ID="ID_1366398058" CREATED="1442738255030" MODIFIED="1442741854195" LINK="http://asciidoc.org/userguide.html#X100"/>
+<node TEXT="compare with AsciiDoctor (Ruby) modularity" ID="ID_708943099" CREATED="1442738175423" MODIFIED="1442738200587"/>
+</node>
+<node TEXT="themes" ID="ID_781495200" CREATED="1442742270243" MODIFIED="1442742552739" LINK="http://asciidoc.org/userguide.html#X99">
+<node TEXT="CSS files can be directly taken from AsciiDoctor to get at least similar look" ID="ID_1539693613" CREATED="1442742276963" MODIFIED="1442742604615"/>
+</node>
+<node TEXT="encoding" ID="ID_509731877" CREATED="1442743868004" MODIFIED="1442743878744">
+<node TEXT="command line" ID="ID_168762319" CREATED="1442743894251" MODIFIED="1442743921392"/>
+<node TEXT=":encoding:" ID="ID_1590236449" CREATED="1442743921965" MODIFIED="1442743928384"/>
+<node TEXT="If the header contains non-UTF-8 characters then the encoding must precede the header (either in the document or on the command-line)." ID="ID_980477605" CREATED="1442743880725" MODIFIED="1442743891408"/>
+</node>
+</node>
 <node TEXT="structure of asciidoc.py (Python 2 implementation)" POSITION="right" ID="ID_1273295101" CREATED="1442487874781" MODIFIED="1442487940461">
 <edge COLOR="#ff0000" WIDTH="2"/>
 <node TEXT="Program constants." ID="ID_1149030057" CREATED="1442488073837" MODIFIED="1442488077259"/>
@@ -89,7 +104,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 <node TEXT="userdir" ID="ID_1854943374" CREATED="1442519931574" MODIFIED="1442519946603"/>
@@ -143,6 +157,32 @@
 <node TEXT="Functions" ID="ID_1952826778" CREATED="1442488273723" MODIFIED="1442488276988"/>
 </node>
 <node TEXT="Main body" ID="ID_1216713732" CREATED="1442488278611" MODIFIED="1442488286189"/>
+</node>
+<node TEXT="fix for encodings" POSITION="right" ID="ID_1512124338" CREATED="1442744012059" MODIFIED="1442744041079">
+<edge COLOR="#ff00ff" WIDTH="2"/>
+<node TEXT="encoding from command line" ID="ID_186666389" CREATED="1442744042547" MODIFIED="1442744061935">
+<node TEXT="--attribute encoding=windows-1250" ID="ID_1263596308" CREATED="1442744478953" MODIFIED="1442744501708">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node TEXT=":encoding: in the source" ID="ID_250381512" CREATED="1442744062555" MODIFIED="1442744072955">
+<node TEXT="tested :encoding: windows-1250" ID="ID_408714820" CREATED="1442744527778" MODIFIED="1442744601437">
+<icon BUILTIN="button_cancel"/>
+<icon BUILTIN="messagebox_warning"/>
+<node TEXT="As Python 3 port uses text mode for opening procesed files, the file has to be reopened with the given encoding." ID="ID_746191329" CREATED="1442744072964" MODIFIED="1442744166626"/>
+</node>
+</node>
+</node>
+<node TEXT="plan for refactoring" POSITION="right" ID="ID_502023297" CREATED="1442736647395" MODIFIED="1442736682201">
+<edge COLOR="#0000ff" WIDTH="2"/>
+<node TEXT="removing global variables" ID="ID_1760261855" CREATED="1442736691037" MODIFIED="1442736704960">
+<node TEXT="document" ID="ID_1334485779" CREATED="1442736706885" MODIFIED="1442736713697"/>
+</node>
+<node TEXT="functions that work with global variables..." ID="ID_1789739714" CREATED="1442736788893" MODIFIED="1442736806453">
+<node TEXT="asciidoc()" ID="ID_242970753" CREATED="1442736841644" MODIFIED="1442736869972">
+<node TEXT="load_conffiles()" ID="ID_678390052" CREATED="1442736883148" MODIFIED="1442736890226"/>
+</node>
+</node>
 </node>
 </node>
 </map>
