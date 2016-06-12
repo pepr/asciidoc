8d31320f50898dc5e63df6dcd17b368bd421d59f
Petr Prikryl <prikryl@atlas.cz>
2016-06-12 12:34:30 +0200

    core/g.py as empty singleton for future location of global variables

---
+++ core/g.py
@@ -0,0 +1,5 @@
+"""g stands for Globals.
+
+This is a temporary module that plays the role of a singleton -- to implement
+global variables used by the original AsciiDoc design. The globals should
+disappear after finishing the refactoring."""
